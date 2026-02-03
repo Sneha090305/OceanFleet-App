aa1ad553b00b1188778981493076836c039b8557
Sneha <snehangap@gmail.com>
2026-02-03 14:46:22 +0530

    [Sneha] Added: UC2: Store vessel records using List.

---
+++ src/util/VesselUtil.java
@@ -0,0 +1,22 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.Vessel;
+
+public class VesselUtil {
+
+    private List<Vessel> vesselList = new ArrayList<>();
+
+    public void addVesselPerformance(Vessel vessel) {
+        vesselList.add(vessel);
+    }
+
+    public List<Vessel> getVesselList() {
+        return vesselList;
+    }
+
+    public void setVesselList(List<Vessel> vesselList) {
+        this.vesselList = vesselList;
+    }
+}
